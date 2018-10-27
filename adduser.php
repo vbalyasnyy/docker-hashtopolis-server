be76cc436a9c68c20ddb9cea33ede75a43a0943c
Pascal Schneider <darkgigabyte@googlemail.com>
2018-10-27 04:18:53 +0200

    refactored global factories variable in adduser

--- adduser.php
+++ adduser.php
@@ -6,6 +6,7 @@ use DBA\Config;
 use DBA\QueryFilter;
 use DBA\RightGroup;
 use DBA\User;
+use DBA\Factory;
 
 require_once(dirname(__FILE__) . "/../inc/load.php");
 
@@ -13,25 +14,25 @@ require_once(dirname(__FILE__) . "/../inc/load.php");
     $password = "H8_PASS";
     $email = "H8_EMAIL";
     
-    if ($FACTORIES::getUserFactory()->getDB(true) === null) {
+    if (Factory::getUserFactory()->getDB(true) === null) {
       //connection not valid
 	printf ( "Unable to connect to the Database\n" );
         exit;
     }
 
-    $FACTORIES::getAgentFactory()->getDB()->beginTransaction();
+    Factory::getAgentFactory()->getDB()->beginTransaction();
     
     $qF = new QueryFilter(RightGroup::GROUP_NAME, "Administrator", "=");
-    $group = $FACTORIES::getRightGroupFactory()->filter(array($FACTORIES::FILTER => array($qF)));
+    $group = Factory::getRightGroupFactory()->filter(array(Factory::FILTER => array($qF)));
     $group = $group[0];
     $newSalt = Util::randomString(20);
 //    $newHash = Encryption::passwordHash($password, $newSalt);
     $user = new User(0, $username, $email, Encryption::passwordHash($password, $newSalt), $newSalt, 1, 1, 0, time(), 3600, $group->getId(), 0, "", "", "", "");
-    $FACTORIES::getUserFactory()->save($user);
+    Factory::getUserFactory()->save($user);
     
     // create default group
     $group = AccessUtils::getOrCreateDefaultAccessGroup();
     $groupUser = new AccessGroupUser(0, $group->getId(), $user->getId());
-    $FACTORIES::getAccessGroupUserFactory()->save($groupUser);
-    $FACTORIES::getAgentFactory()->getDB()->commit();
+    Factory::getAccessGroupUserFactory()->save($groupUser);
+    Factory::getAgentFactory()->getDB()->commit();
 
