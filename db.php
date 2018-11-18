9012566e50bce9d8913bc50aadc495feb5dbdc0d
Pascal Schneider <darkgigabyte@googlemail.com>
2018-11-18 14:43:53 +0100

    Update db.php

--- db.php
+++ db.php
@@ -5,7 +5,14 @@ $CONN['user'] = 'MYSQL_USER';
 $CONN['pass'] = 'MYSQL_PASSWORD';
 $CONN['server'] = 'MYSQL_HOST';
 $CONN['db'] = 'MYSQL_DB';
-$CONN['port'] = '3306';
 $CONN['port'] = 'MYSQL_PORT';
+
+$PEPPER = [
+  "__PEPPER1__",
+  "__PEPPER2__",
+  "__PEPPER3__",
+  "__CSRF__"
+];
+
 $INSTALL = PENDING; //set this to true if you config the mysql and setup manually
 //END CONFIG
