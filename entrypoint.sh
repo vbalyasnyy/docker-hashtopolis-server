7e6682415b16f71bd7c2cbd6e3a47fc5952d9551
Vasilyy Balyasnyy <v.balyasnyy@gmail.com>
2020-12-23 21:51:59 +0300

    [entrypoint.sh] Use more compact syntax.
    Signed-off-by: Vasilyy Balyasnyy <v.balyasnyy@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,59 +1,25 @@
 #!/bin/bash
 
-#	Try to connect to mysql 3 times
-ATTEMPTS=12
-#	Wait 5 secconds before trying to reconnect
-INTERVAL=5
+ATTEMPTS=12 # try to connect to mysql 
+INTERVAL=5  # seconds before trying to reconnect
 
 function getRandom() {
 	dd if=/dev/urandom bs=32768 count=1 2>/dev/null | openssl sha512  | grep stdin | cut -d " " -f2 | cut -c1-64
 }
 
-if [ -n "$MYSQL_ENV_MYSQL_ROOT_PASSWORD" ]
-then
-	MYSQL_ROOT_PASSWORD="$MYSQL_ENV_MYSQL_ROOT_PASSWORD"
-fi
+function exitOnFail() {
+    [ $? -ne 0 ] && echo "Error: ${1}" && exit 7
+}
 
-if [ -n "$MYSQL_ENV_MYSQL_USER" ]
-then
-	MYSQL_USER="$MYSQL_ENV_MYSQL_USER"
-fi
-
-if [ -n "$MYSQL_ENV_MYSQL_PASSWORD" ]
-then
-	MYSQL_PASSWORD="$MYSQL_ENV_MYSQL_PASSWORD"
-fi
-
-if [ -n "$MYSQL_ENV_MYSQL_DATABASE" ]
-then
-	MYSQL_DB="$MYSQL_ENV_MYSQL_DATABASE"
-fi
-
-if [ -n "$MYSQL_PORT_3306_TCP_ADDR" ]
-then
-	MYSQL_HOST="$MYSQL_PORT_3306_TCP_ADDR"
-fi
-
-if [ -z "$MYSQL_HOST" ]
-then
-	MYSQL_HOST="mysql"
-fi
-
-if [ -z "$MYSQL_DB" ]
-then
-	MYSQL_DB="hashtopolis"
-fi
-
-if [ -z "$MYSQL_PORT" ]
-then
-	MYSQL_PORT="3306"
-fi
-
-if [ ! -z "$MYSQL_ROOT_PASSWORD" ]
-then
-	MYSQL_USER="root"
-	MYSQL_PASSWORD=$MYSQL_ROOT_PASSWORD
-fi
+[ -n "$MYSQL_ENV_MYSQL_ROOT_PASSWORD" ] && MYSQL_ROOT_PASSWORD="$MYSQL_ENV_MYSQL_ROOT_PASSWORD"
+[ -n "$MYSQL_ENV_MYSQL_USER" ]          && MYSQL_USER="$MYSQL_ENV_MYSQL_USER"
+[ -n "$MYSQL_ENV_MYSQL_PASSWORD" ]      && MYSQL_PASSWORD="$MYSQL_ENV_MYSQL_PASSWORD"
+[ -n "$MYSQL_ENV_MYSQL_DATABASE" ]      && MYSQL_DB="$MYSQL_ENV_MYSQL_DATABASE"
+[ -n "$MYSQL_PORT_3306_TCP_ADDR" ]      && MYSQL_HOST="$MYSQL_PORT_3306_TCP_ADDR"
+[ -z "$MYSQL_HOST" ]                    && MYSQL_HOST="mysql"
+[ -z "$MYSQL_DB" ]                      && MYSQL_DB="hashtopolis"
+[ -z "$MYSQL_PORT" ]                    && MYSQL_PORT="3306"
+[ -n "$MYSQL_ROOT_PASSWORD" ]           && MYSQL_USER="root" && MYSQL_PASSWORD=$MYSQL_ROOT_PASSWORD
 
 if [ ! -f /var/www/html/inc/conf.php ]
 then
@@ -62,93 +28,63 @@ then
 	chown -R www-data:www-data /var/www/html
 fi
 
-if grep "PENDING" /var/www/html/inc/conf.php &>/dev/null
+if [ ! grep "PENDING" /var/www/html/inc/conf.php &>/dev/null ]
 then
-#	CHECK MYSQL AVAILABILITY
-	MYSQL="mysql -u$MYSQL_USER -p$MYSQL_PASSWORD -h$MYSQL_HOST"
-	$MYSQL -e "SELECT 'PING';" &>/dev/null
-	ERROR=$?
+    /usr/sbin/apachectl -DFOREGROUND
+    exit 0
+fi
 
-	while [ $ERROR -ne 0 -a $ATTEMPTS -gt 1 ]
-	do
-		ATTEMPTS=$(($ATTEMPTS-1))
-		echo "Failed connecting to the database.... Sleeping 5s and retrying $ATTEMPTS more."
-		sleep $INTERVAL
-		$MYSQL -e "SELECT 'PING';" &>/dev/null
-		ERROR=$?
-	done
+# CHECK MYSQL AVAILABILITY
+MYSQL="mysql -u$MYSQL_USER -p$MYSQL_PASSWORD -h$MYSQL_HOST"
 
-	if [ $ERROR -ne 0 ]
-	then
-		echo "Could not connect to mysql. Please double check your settings and mysql's availability."
-		echo "Used: $MYSQL"
-		exit 20
-	fi
-	
-	if [ -z "$DO_NOT_SEED" ]
-	then
-#	CREATE DB
-		$MYSQL -e "CREATE database $MYSQL_DB;"
-#	APPEND DB
-		MYSQL="$MYSQL $MYSQL_DB"
-		if [ $? -ne 0 ]
-		then
-			echo "Failed to create the database... insufficient access??? already exists??? this shouldn't happen, I'm doing setup..."
-			exit 21
-		fi
-#	IMPORT DB
-		$MYSQL < /var/www/html/install/hashtopolis.sql
-		if [ $? -ne 0 ]
-		then
-			echo "DB Import Failed!!!"
-			exit 12
-		fi
-	fi
-#	CONFIGURE DB
-#	RUN SETUP & ADD USER
+$MYSQL -e "SELECT 'PING';" &>/dev/null
+ERROR=$?
+
+while [ $ERROR -ne 0 -a $ATTEMPTS -gt 1 ]
+do
+    ATTEMPTS=$(($ATTEMPTS-1))
+    echo "Failed connecting to the database.... Sleeping ${INTERVAL} sec and retrying $ATTEMPTS more."
+    sleep $INTERVAL
+    $MYSQL -e "SELECT 'PING';" &>/dev/null
+    ERROR=$?
+done
+
+if [ $ERROR -ne 0 ]
+then
+    echo "Could not connect to mysql. Please double check your settings and mysql's availability."
+    exit 20
+fi
+
+# CREATE DB
+$MYSQL -e "CREATE database $MYSQL_DB;"
+exitOnFail "Failed to create the database ${MYSQL_DB}."
+
+# APPEND DB
+MYSQL="$MYSQL $MYSQL_DB"
+
+# IMPORT DB
+$MYSQL < /var/www/html/install/hashtopolis.sql
+exitOnFail "DB Import Failed!!!"
+
+# CONFIGURE DB
+# RUN SETUP & ADD USER
 sed -i -e "s/MYSQL_USER/$MYSQL_USER/" -e "s/MYSQL_PASSWORD/$MYSQL_PASSWORD/" -e "s/MYSQL_DB/$MYSQL_DB/" -e "s/MYSQL_HOST/$MYSQL_HOST/" -e "s/PENDING/true/" /var/www/html/inc/conf.php || exit 8
 #	-e "s/MYSQL_PORT/$MYSQL_PORT/"  <--- fails and I don't get why...
 /usr/bin/php /var/www/html/install/setup.php
-#	CREATE USER & PASSWORD
-if [ -z "$H8_USER" ]
-then
 
-	H8_USER=$(getRandom)
-	echo -e "No login provided, generating random username:\n\t$H8_USER\n\nIf you don't like it, check the docs first, this is for your own security. admin/admin on a cracking tool sounds a bit funny.\n"
-fi
-
-if [ -z "$H8_PASS" ]
-then
-	H8_PASS=$(getRandom)
-	echo -e "Your random password is: $H8_PASS\n\n\n"
-fi
-
+# CREATE USER & PASSWORD
+[ -z "$H8_USER" ] && H8_USER=$(getRandom) && echo -e "Your random username:\n\t$H8_USER\n\n\n"
+[ -z "$H8_PASS" ] && H8_PASS=$(getRandom) && echo -e "Your random password:\n\t$H8_PASS\n\n\n"
 sed -i -e "s/H8_USER/$H8_USER/" -e  "s/H8_PASS/$H8_PASS/" -e "s/H8_EMAIL/$H8_EMAIL/" /var/www/html/install/adduser.php
-if [ -z "$DO_NOT_SEED" ]
-then
-	/usr/bin/php /var/www/html/install/adduser.php
-fi
-#	PHP MAIL SETTINGS
-if [ -n "$PHP_MAIL_HOST" ]
-then
-	sed -i "s/^SMTP.*/SMTP = $PHP_MAIL_HOST/" /etc/php/7.0/apache2/php.ini
-fi
+/usr/bin/php /var/www/html/install/adduser.php
 
-
-if [ -n "$PHP_MAIL_PORT" ]
-then
-	sed -i "s/^smtp_port.*/smtp_port = $PHP_MAIL_PORT/" /etc/php/7.0/apache2/php.ini
-fi
-
-
-if [ -n "$PHP_MAIL_FROM" ]
-then
-	sed -i "s/^;sendmail_from.*/sendmail_from = $PHP_MAIL_FROM/" /etc/php/7.0/apache2/php.ini
-fi
-
+# PHP MAIL SETTINGS
+[ -n "$PHP_MAIL_HOST" ] && sed -i "s/^SMTP.*/SMTP = $PHP_MAIL_HOST/" /etc/php/7.0/apache2/php.ini
+[ -n "$PHP_MAIL_PORT" ] && sed -i "s/^smtp_port.*/smtp_port = $PHP_MAIL_PORT/" /etc/php/7.0/apache2/php.ini
+[ -n "$PHP_MAIL_FROM" ] && sed -i "s/^;sendmail_from.*/sendmail_from = $PHP_MAIL_FROM/" /etc/php/7.0/apache2/php.ini
 
 echo "Setup finished, pruning /install folder"
-	rm -rf /var/www/html/install
-fi
+rm -rf /var/www/html/install
 
 /usr/sbin/apachectl -DFOREGROUND
+
